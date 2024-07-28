505f54a648c3f400c8b0bc6e2c2531267bd7cc10
dhiraj1996 <dhirajkumar1996@gmail.com>
2024-07-28 19:30:38 +0530

    navbar creation

---
+++ navigationBar/src/App.jsx
@@ -0,0 +1,12 @@
+import Nav from "./components/Nav";
+
+export default function App() {
+  return (
+    <>
+      <Nav></Nav>
+      <h1 className="text-3xl font-bold underline text-center text-secondary">
+        Hello world!
+      </h1>
+    </>
+  )
+}
